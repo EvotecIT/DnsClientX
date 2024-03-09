041626420bf49d7ee8989ed732c806ccc91a3a1e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-09 23:24:00 +0100

    Add json

---
+++ DnsClientX/ProtocolDnsJson/DnsJson.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DnsClientX {
+    internal static class DnsJson {
+        /// <summary>
+        /// Encode URL
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string UrlEncode(this string value) => WebUtility.UrlEncode(value);
+
+        /// <summary>
+        /// Deserialize a JSON HTTP response into a given type.
+        /// </summary>
+        /// <typeparam name="T">The type to deserialize into.</typeparam>
+        /// <param name="response">The HTTP response message with JSON as a body.</param>
+        /// <param name="debug">Whether to print the JSON data to the console.</param>
+        public static async Task<T> Deserialize<T>(this HttpResponseMessage response, bool debug = false) {
+            using Stream stream = await response.Content.ReadAsStreamAsync();
+            if (stream.Length == 0) throw new DnsClientException("Response content is empty, can't parse as JSON.");
+            try {
+                if (debug) {
+                    // Read the stream as a string
+                    StreamReader reader = new StreamReader(stream);
+                    string json = await reader.ReadToEndAsync();
+                    // Print the JSON data to the console
+                    Console.WriteLine(json);
+                    // Deserialize the JSON data
+                    return JsonSerializer.Deserialize<T>(json);
+                }
+                return JsonSerializer.Deserialize<T>(stream);
+            } catch (JsonException jsonEx) {
+                throw new DnsClientException($"Failed to parse JSON due to a JsonException: {jsonEx.Message}");
+            } catch (IOException ioEx) {
+                throw new DnsClientException($"Failed to read the response stream due to an IOException: {ioEx.Message}");
+            } catch (Exception ex) {
+                throw new DnsClientException($"Unexpected exception while parsing JSON: {ex.GetType().Name} => {ex.Message}");
+            }
+        }
+    }
+}
---
+++ DnsClientX/ProtocolDnsJson/DnsJsonResolve.cs
@@ -0,0 +1,61 @@
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System;
+
+namespace DnsClientX {
+    internal static class DnsJsonResolve {
+        /// <summary>
+        /// Sends a DNS query in JSON format and returns the response.
+        /// </summary>
+        /// <param name="client">The HttpClient used to send the request.</param>
+        /// <param name="name">The domain name to query.</param>
+        /// <param name="type">The type of DNS record to query.</param>
+        /// <param name="requestDnsSec">If set to <c>true</c>, the method will request DNSSEC data in the response.</param>
+        /// <param name="validateDnsSec">If set to <c>true</c>, the method will validate DNSSEC data.</param>
+        /// <param name="debug">If set to <c>true</c>, the method will include debugging information in the response.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the DNS response.</returns>
+        /// <exception cref="DnsClientException">Thrown when the HTTP request fails or the server returns an error.</exception>
+        internal static async Task<DnsResponse> ResolveJsonFormat(this HttpClient client, string name, DnsRecordType type, bool requestDnsSec, bool validateDnsSec, bool debug) {
+            //var uriBuilder = new UriBuilder(client.BaseAddress);
+            //var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            //query["name"] = name;
+            //if (type != DnsRecordType.A) query["type"] = type.ToString();
+            //if (requestDnsSec) query["do"] = "1";
+            //if (validateDnsSec) query["cd"] = "1";
+            //uriBuilder.Query = query.ToString();
+            //string url = uriBuilder.ToString();
+
+            string url = string.Concat($"?name={name.UrlEncode()}", type == DnsRecordType.A ? "" : $"&type={type.ToString().UrlEncode()}", requestDnsSec == false ? "" : $"&do=1", validateDnsSec == false ? "" : $"&cd=1");
+
+            using HttpRequestMessage req = new(HttpMethod.Get, url);
+            try {
+                using HttpResponseMessage res = await client.SendAsync(req);
+
+                DnsResponse response = await res.Deserialize<DnsResponse>(debug);
+
+                if (res.StatusCode != HttpStatusCode.OK || !string.IsNullOrEmpty(response.Error)) {
+                    string message = $"Failed to query type {type} of \"{name}\", received HTTP status code {res.StatusCode}.";
+                    if (!string.IsNullOrEmpty(response.Error)) message += $"\nError: {response.Error}";
+                    if (response.Comments != null) message += $"\nComments: {string.Join(", ", response.Comments)}";
+
+                    throw new DnsClientException(message, response);
+                }
+
+                return response;
+            } catch (HttpRequestException ex) {
+                string message = $"Failed to send HTTP request for type {type} of '{name}'.";
+                message += $" Error: {ex.Message}";
+                if (ex.InnerException?.Message != null) {
+                    message += $" Inner Exception: {ex.InnerException.Message}";
+                }
+                throw new DnsClientException(message);
+            } catch (JsonException jsonEx) {
+                throw new DnsClientException($"Failed to parse JSON response for type {type} of '{name}'. Error: {jsonEx.Message}");
+            } catch (Exception ex) {
+                throw new DnsClientException($"Unexpected exception while querying type {type} of '{name}'. Error: {ex.Message}");
+            }
+        }
+    }
+}
