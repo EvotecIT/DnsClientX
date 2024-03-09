92e449ccdabf4b8b93de0d348ba3a2d55138f6ad
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-09 23:23:37 +0100

    Trying DOT still not working

--- DnsClientX/ProtocolDnsWire/DnsWireResolveDot.cs
+++ DnsClientX/ProtocolDnsWire/DnsWireResolveDot.cs
@@ -7,11 +7,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DnsClientX {
-    /// <summary>
-    ///
-    /// TODO - This method is not yet working correctly
-    /// </summary>
     internal static class DnsWireResolveDot {
+        /// <summary>
+        ///
+        /// TODO - This method is not yet working correctly
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="type">The type.</param>
+        /// <param name="requestDnsSec">if set to <c>true</c> [request DNS sec].</param>
+        /// <param name="validateDnsSec">if set to <c>true</c> [validate DNS sec].</param>
+        /// <param name="debug">if set to <c>true</c> [debug].</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">name - Name is null or empty.</exception>
+        /// <exception cref="System.Exception">
+        /// Failed to read the length prefix of the response.
+        /// or
+        /// The stream was closed before the entire response could be read.
+        /// </exception>
         internal static async Task<DnsResponse> ResolveWireFormatDoT(string name, DnsRecordType type, bool requestDnsSec, bool validateDnsSec, bool debug = false) {
             if (string.IsNullOrEmpty(name)) throw new ArgumentNullException(nameof(name), "Name is null or empty.");
 
@@ -31,35 +43,47 @@ namespace DnsClientX {
 
             if (debug) {
                 // Print the combined DNS query bytes to the console
+                Console.WriteLine($"Query Name: " + name + " type: " + type);
+                Console.WriteLine($"Query before combination: {BitConverter.ToString(queryBytes)}");
                 Console.WriteLine($"Sending combined query: {BitConverter.ToString(combinedQueryBytes)}");
             }
 
-
-            if (debug) {
-                // Print the DNS wire format bytes to the console
-                Console.WriteLine("Query    Name: " + name + " type: " + type);
-                Console.WriteLine($"Sending query: {BitConverter.ToString(queryBytes)}");
-            }
-
-            string dnsServer = "1.1.1.1";
+            string dnsServer = "1.1.1.1"; // Cloudflare's DoT endpoint
             int port = 853;
-            var sslValidationCallback = new RemoteCertificateValidationCallback((sender, certificate, chain, sslPolicyErrors) => true);
+
             using (var client = new TcpClient(dnsServer, port))
-            using (var sslStream = new SslStream(client.GetStream(), false, sslValidationCallback)) {
-                //await sslStream.AuthenticateAsClientAsync(dnsServer);
+            using (var sslStream = new SslStream(client.GetStream(), false, (sender, certificate, chain, sslPolicyErrors) => true)) {
                 await sslStream.AuthenticateAsClientAsync(dnsServer, null, SslProtocols.Tls12, false);
 
                 await sslStream.WriteAsync(combinedQueryBytes, 0, combinedQueryBytes.Length);
                 await sslStream.FlushAsync();
 
-                // Read response
-                var buffer = new byte[4096];
-                int bytesRead = await sslStream.ReadAsync(buffer, 0, buffer.Length);
-                byte[] responseBytes = new byte[bytesRead];
-                Array.Copy(buffer, responseBytes, bytesRead);
+                // Prepare to read the response with handling for length prefix
+                var lengthPrefixBuffer = new byte[2];
+                int prefixBytesRead = await sslStream.ReadAsync(lengthPrefixBuffer, 0, 2);
+                if (prefixBytesRead != 2) {
+                    throw new Exception("Failed to read the length prefix of the response.");
+                }
+                int responseLength = (lengthPrefixBuffer[0] << 8) + lengthPrefixBuffer[1]; // Calculate total response length
+
+                var responseBuffer = new byte[responseLength];
+                int totalBytesRead = 0;
+                while (totalBytesRead < responseLength) {
+                    int bytesRead = await sslStream.ReadAsync(responseBuffer, totalBytesRead, responseLength - totalBytesRead);
+                    if (bytesRead == 0) {
+                        throw new Exception("The stream was closed before the entire response could be read.");
+                    }
+                    totalBytesRead += bytesRead;
+                }
+
+                // At this point, responseBuffer contains the full DNS response
+                if (debug) {
+
+                    Console.WriteLine($"Received response: {BitConverter.ToString(responseBuffer)}");
+                }
 
                 // Deserialize the response from DNS wire format
-                var response = await DnsWire.DeserializeDnsWireFormat(null, debug, responseBytes);
+                var response = await DnsWire.DeserializeDnsWireFormat(null, debug, responseBuffer);
                 return response;
             }
         }
