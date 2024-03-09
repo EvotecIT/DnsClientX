1a9c87d3a888d5f417c0f1178b776511ab8a39e7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-09 23:55:52 +0100

    Updae POST

--- DnsClientX/ProtocolDnsWire/DnsWireResolvePost.cs
+++ DnsClientX/ProtocolDnsWire/DnsWireResolvePost.cs
@@ -8,16 +8,19 @@ using System.Threading.Tasks;
 namespace DnsClientX {
     internal static class DnsWireResolvePost {
         /// <summary>
-        /// Resolves the wire format post.
-        /// TODO - This method is not yet working correctly
+        /// Sends a DNS query in wire format using HTTP POST and returns the response.
+        ///
+        /// TODO: This method is not yet implemented.
         /// </summary>
-        /// <param name="name">The name.</param>
-        /// <param name="type">The type.</param>
-        /// <param name="requestDnsSec">if set to <c>true</c> [request DNS sec].</param>
-        /// <param name="validateDnsSec">if set to <c>true</c> [validate DNS sec].</param>
-        /// <returns></returns>
-        /// <exception cref="System.ArgumentNullException">name - Name is null or empty.</exception>
-        /// <exception cref="DnsClientException">HTTP request failed with status code: {response.StatusCode}</exception>
+        /// <param name="client">The HttpClient used to send the request.</param>
+        /// <param name="name">The domain name to query.</param>
+        /// <param name="type">The type of DNS record to query.</param>
+        /// <param name="requestDnsSec">If set to <c>true</c>, the method will request DNSSEC data in the response.</param>
+        /// <param name="validateDnsSec">If set to <c>true</c>, the method will validate DNSSEC data.</param>
+        /// <param name="debug">If set to <c>true</c>, the method will include debugging information in the response.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the DNS response.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the 'name' parameter is null or empty.</exception>
+        /// <exception cref="DnsClientException">Thrown when the HTTP request fails or the server returns an error.</exception>
         internal static async Task<DnsResponse> ResolveWireFormatPost(this HttpClient client, string name, DnsRecordType type, bool requestDnsSec, bool validateDnsSec, bool debug = false) {
             if (string.IsNullOrEmpty(name)) throw new ArgumentNullException(nameof(name), "Name is null or empty.");
 
