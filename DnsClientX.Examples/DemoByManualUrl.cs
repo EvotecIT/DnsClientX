9273934ab100941bd66b1d796707b48e17842265
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-09 21:46:41 +0100

    update examples

--- DnsClientX.Examples/DemoByManualUrl.cs
+++ DnsClientX.Examples/DemoByManualUrl.cs
@@ -14,5 +14,14 @@ namespace DnsClientX.Examples {
             var data = await client.Resolve("evotec.pl", DnsRecordType.A);
             data.Answers.DisplayToConsole();
         }
+
+        // TODO - This method is not yet working correctly
+        public static async Task ExampleTesting() {
+            DnsClientX client = new DnsClientX(new Uri("https://1.1.1.1/dns-query"), DnsRequestFormat.WireFormatDot) {
+                Debug = true
+            };
+            var data = await client.Resolve("www.example.com", DnsRecordType.A);
+            data.Answers.DisplayToConsole();
+        }
     }
 }
--- DnsClientX.Examples/DemoQuery.cs
+++ DnsClientX.Examples/DemoQuery.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 namespace DnsClientX.Examples {
     internal class DemoQuery {
         public static async Task Example1() {
-            var data = await DnsClientX.QueryDns("evotec.pl", DnsRecordType.A, DnsEndpoint.Cloudflare);
+            var data = await DnsClientX.QueryDns("evotec.pl", DnsRecordType.A, DnsEndpoint.CloudflareWireFormat);
             data.Answers.DisplayToConsole();
         }
         public static async Task Example2() {
@@ -17,6 +17,7 @@ namespace DnsClientX.Examples {
             data.Answers.DisplayToConsole();
         }
 
+        // TODO - This method is not yet working correctly
         public static async Task ExampleTesting() {
             var data = await DnsClientX.QueryDns("evotec.pl", DnsRecordType.A, new Uri("https://1.1.1.1/dns-query"), DnsRequestFormat.WireFormatPost);
             data.Answers.DisplayToConsole();
