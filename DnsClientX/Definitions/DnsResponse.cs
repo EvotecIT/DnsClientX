b7e04900c7e64d50f8ca59d1b0acc3572b053893
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-13 19:17:07 +0100

    Fix defnition, set is required public for json

--- DnsClientX/Definitions/DnsResponse.cs
+++ DnsClientX/Definitions/DnsResponse.cs
@@ -10,78 +10,79 @@ namespace DnsClientX {
         /// The status code of the DNS response.
         /// </summary>
         [JsonPropertyName("Status")]
-        public DnsResponseCode Status { get; internal set; }
+        public DnsResponseCode Status { get; set; }
 
         /// <summary>
         /// Indicates whether the response was truncated. This can occur if the response is larger than the maximum size allowed by the transmission channel.
         /// This is typically false for DNS over HTTPS, as most providers support the maximum response size.
         /// </summary>
         [JsonPropertyName("TC")]
-        public bool IsTruncated { get; internal set; }
+        public bool IsTruncated { get; set; }
 
         /// <summary>
         /// Indicates whether recursion was requested in the query. This is typically true for DNS over HTTPS.
         /// </summary>
         [JsonPropertyName("RD")]
-        public bool IsRecursionDesired { get; internal set; }
+        public bool IsRecursionDesired { get; set; }
 
         /// <summary>
         /// Indicates whether recursion is available on the DNS server. This is typically true for DNS over HTTPS.
         /// </summary>
         [JsonPropertyName("RA")]
-        public bool IsRecursionAvailable { get; internal set; }
+        public bool IsRecursionAvailable { get; set; }
 
         /// <summary>
         /// Indicates whether the DNS server believes the response is authentic and has been validated by DNSSEC.
         /// </summary>
         [JsonPropertyName("AD")]
-        public bool AuthenticData { get; internal set; }
+        public bool AuthenticData { get; set; }
 
         /// <summary>
         /// Indicates whether a security-aware resolver should disable DNSSEC signature validation.
         /// </summary>
         [JsonPropertyName("CD")]
-        public bool CheckingDisabled { get; internal set; }
+        public bool CheckingDisabled { get; set; }
 
         /// <summary>
         /// The questions that were asked in the DNS query.
         /// </summary>
         [JsonPropertyName("Question")]
-        public DnsQuestion[] Questions { get; internal set; }
+        public DnsQuestion[] Questions { get; set; }
 
         /// <summary>
         /// The answers provided by the DNS server.
         /// </summary>
         [JsonPropertyName("Answer")]
-        public DnsAnswer[] Answers { get; internal set; }
+        public DnsAnswer[] Answers { get; set; }
 
         /// <summary>
         /// Gets the answers in their minimal form
         /// </summary>
+        [JsonIgnore]
         public DnsAnswerMinimal[] AnswersMinimal => Answers.Select(answer => (DnsAnswerMinimal)answer).ToArray();
 
         /// <summary>
         /// The authority records provided by the DNS server.
         /// </summary>
         [JsonPropertyName("Authority")]
-        public DnsAnswer[] Authorities { get; internal set; }
+        public DnsAnswer[] Authorities { get; set; }
 
         /// <summary>
         /// Any additional records provided by the DNS server.
         /// </summary>
         [JsonPropertyName("Additional")]
-        public DnsAnswer[] Additional { get; internal set; }
+        public DnsAnswer[] Additional { get; set; }
 
         /// <summary>
         /// An error message, if there was an issue with the DNS query. This is typically included when the HTTP status code is 400 (Bad Request).
         /// </summary>
         [JsonPropertyName("error")]
-        public string Error { get; internal set; }
+        public string Error { get; set; }
 
         /// <summary>
         /// An extended DNS error code message. For more information, see the <a href="https://developers.cloudflare.com/1.1.1.1/infrastructure/extended-dns-error-codes/">Cloudflare documentation</a>.
         /// </summary>
         [JsonPropertyName("Comment")]
-        public string Comments { get; internal set; }
+        public string Comments { get; set; }
     }
 }
