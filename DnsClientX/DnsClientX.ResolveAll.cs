17ad3a4cf6f283508aa90717ef9c38e90003c55f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-24 15:04:04 +0100

    Move to different files

---
+++ DnsClientX/DnsClientX.ResolveAll.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DnsClientX {
+    public partial class ClientX {
+
+    }
+}
