014bbd8ae19add56ccfa372e6b62d67ffce214e4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-04 23:15:12 +0200

    Update examples

--- Module/Examples/Example.Dns.ps1
+++
@@ -1,7 +0,0 @@
-﻿Import-Module .\PowerDNSClient.psd1 -Force -Verbose
-
-Resolve-DnsQuery -Name 'evotec.pl' -Type A -DnsProvider Cloudflare | Format-Table
-
-Resolve-DnsQuery -Name 'evotec.pl' -Type TXT -DnsProvider Cloudflare | Format-Table
-
-Resolve-DnsQuery -Name 'github.com' -Type TXT -DnsProvider Cloudflare | Format-Table
