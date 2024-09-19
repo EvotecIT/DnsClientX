134f54460115410ca95fe2c1b23d0d19b0a8bc76
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-19 20:20:03 +0200

    Update examples

--- Module/Examples/Example.ResolveDnsQuery.ps1
+++ Module/Examples/Example.ResolveDnsQuery.ps1
@@ -3,8 +3,10 @@
 Import-Module $PSScriptRoot\..\DnsClientX.psd1 -Force
 
 Resolve-DnsQuery -Name 'www.bücher.de' -Type A | Format-Table
-
 Resolve-DnsQuery -Name 'evotec.pl' -Type A | Format-Table
 Resolve-DnsQuery -Name 'evotec.pl' -Type A -DnsProvider Cloudflare -Verbose | Format-Table
 Resolve-DnsQuery -Name 'evotec.pl' -Type TXT -DnsProvider System -Verbose | Format-Table
 Resolve-DnsQuery -Name 'github.com', 'evotec.pl', 'google.com' -Type TXT -DnsProvider System -Verbose | Format-Table
+Resolve-DnsQuery -Name 'github.com', 'evotec.pl', 'google.com' -Type TXT -DnsProvider System -Verbose #-TimeOut 5000
+Resolve-DnsQuery -Name 'evotec.pl' -Type SOA -Server 8.8.4.1 -Verbose -TimeOut 500 | Format-Table
+Resolve-DnsQuery -Name 'evotec.pl' -Type SOA -Server a1.net -Verbose -TimeOut 500 | Format-Table
