949157d4ccf2b54e17bd21e844862456da56d0a4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-25 17:22:48 +0100

    Add additional example methods for PTR DNS queries in Program.cs

--- DnsClientX.Examples/Program.cs
+++ DnsClientX.Examples/Program.cs
@@ -3,7 +3,10 @@ using System.Threading.Tasks;
 namespace DnsClientX.Examples {
     public static class Program {
         public static async Task Main() {
-            await DemoQuery.ExamplePTR(); 
+            await DemoQuery.ExamplePTR();
+            await DemoQuery.ExamplePTR1();
+            await DemoQuery.ExamplePTR2();
+            await DemoQuery.ExamplePTR3();
             return;
 
             // await ConvertToDnsClient.ExampleConvertToDnsClientFromX();
