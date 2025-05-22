6224c20ae1485441af6fdc61695b0dbaf785a436
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-05-22 14:34:45 +0200

    refactor(tests): 🧪 Remove outdated test cases from QueryDnsByUri
    * Removed inline data for specific DNS URIs that are no longer relevant in the test cases for `ShouldWorkForTXT`, `ShouldWorkForTXTSync`, `ShouldWorkForA`, and `ShouldWorkForASync`.
    * This cleanup enhances the clarity and focus of the tests, ensuring they only include currently applicable URIs.

--- DnsClientX.Tests/QueryDnsByUri.cs
+++ DnsClientX.Tests/QueryDnsByUri.cs
@@ -7,7 +7,6 @@ namespace DnsClientX.Tests {
         [Theory]
         [InlineData("https://1.1.1.1/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
         [InlineData("https://8.8.8.8/resolve", DnsRequestFormat.DnsOverHttpsJSON)]
-        [InlineData("https://9.9.9.11:5053/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
         [InlineData("https://208.67.222.123/dns-query", DnsRequestFormat.DnsOverHttps)]
         public async Task ShouldWorkForTXT(string baseUri, DnsRequestFormat requestFormat) {
             Uri uri = new Uri(baseUri);
@@ -22,7 +21,6 @@ namespace DnsClientX.Tests {
         [Theory]
         [InlineData("https://1.1.1.1/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
         [InlineData("https://8.8.8.8/resolve", DnsRequestFormat.DnsOverHttpsJSON)]
-        [InlineData("https://9.9.9.11:5053/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
         [InlineData("https://208.67.222.123/dns-query", DnsRequestFormat.DnsOverHttps)]
         public void ShouldWorkForTXTSync(string baseUri, DnsRequestFormat requestFormat) {
             Uri uri = new Uri(baseUri);
@@ -37,7 +35,6 @@ namespace DnsClientX.Tests {
         [Theory]
         [InlineData("https://1.1.1.1/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
         [InlineData("https://8.8.8.8/resolve", DnsRequestFormat.DnsOverHttpsJSON)]
-        [InlineData("https://9.9.9.10:5053/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
         [InlineData("https://doh.opendns.com/dns-query", DnsRequestFormat.DnsOverHttps)]
         [InlineData("https://dns.adguard.com/dns-query", DnsRequestFormat.DnsOverHttps)]
         public async Task ShouldWorkForA(string baseUri, DnsRequestFormat requestFormat) {
@@ -53,7 +50,6 @@ namespace DnsClientX.Tests {
         [Theory]
         [InlineData("https://1.1.1.1/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
         [InlineData("https://8.8.8.8/resolve", DnsRequestFormat.DnsOverHttpsJSON)]
-        [InlineData("https://9.9.9.10:5053/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
         [InlineData("https://doh.opendns.com/dns-query", DnsRequestFormat.DnsOverHttps)]
         [InlineData("https://dns.adguard.com/dns-query", DnsRequestFormat.DnsOverHttps)]
         public void ShouldWorkForASync(string baseUri, DnsRequestFormat requestFormat) {
