9eb76a5268503012ad216bb52e32bdac07ac5521
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-05-28 07:54:16 +0200

    Update DNS test cases to remove port numbers and switch request format to DnsOverHttps for Quad9 endpoints. This change enhances compatibility with DNS providers.

--- DnsClientX.Tests/QueryDnsByUri.cs
+++ DnsClientX.Tests/QueryDnsByUri.cs
@@ -5,7 +5,7 @@ namespace DnsClientX.Tests {
         [Theory]
         [InlineData("https://1.1.1.1/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
         [InlineData("https://8.8.8.8/resolve", DnsRequestFormat.DnsOverHttpsJSON)]
-        [InlineData("https://9.9.9.11:5053/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
+        [InlineData("https://9.9.9.11/dns-query", DnsRequestFormat.DnsOverHttps)]
         [InlineData("https://208.67.222.123/dns-query", DnsRequestFormat.DnsOverHttps)]
         public async void ShouldWorkForTXT(string baseUri, DnsRequestFormat requestFormat) {
             Uri uri = new Uri(baseUri);
@@ -20,7 +20,7 @@ namespace DnsClientX.Tests {
         [Theory]
         [InlineData("https://1.1.1.1/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
         [InlineData("https://8.8.8.8/resolve", DnsRequestFormat.DnsOverHttpsJSON)]
-        [InlineData("https://9.9.9.10:5053/dns-query", DnsRequestFormat.DnsOverHttpsJSON)]
+        [InlineData("https://9.9.9.10/dns-query", DnsRequestFormat.DnsOverHttps)]
         [InlineData("https://doh.opendns.com/dns-query", DnsRequestFormat.DnsOverHttps)]
         [InlineData("https://dns.adguard.com/dns-query", DnsRequestFormat.DnsOverHttps)]
         public async void ShouldWorkForA(string baseUri, DnsRequestFormat requestFormat) {
