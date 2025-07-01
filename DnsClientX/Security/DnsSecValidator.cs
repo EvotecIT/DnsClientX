f9385276a04936fc9fd5739eda74a0346eecf3c5
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-01 10:27:00 +0200

    Handle spaced Base64 in DNSKEY

--- DnsClientX/Security/DnsSecValidator.cs
+++ DnsClientX/Security/DnsSecValidator.cs
@@ -88,7 +88,7 @@ namespace DnsClientX {
                 return false;
             }
             algorithm = (DnsKeyAlgorithm)algVal;
-            string keyBase64 = string.Join(string.Empty, parts, 3, parts.Length - 3);
+            string keyBase64 = string.Concat(parts.Skip(3));
             try {
                 publicKey = Convert.FromBase64String(keyBase64);
             } catch {
