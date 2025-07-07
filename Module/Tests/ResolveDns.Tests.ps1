383d8d73c18f43ddb5eae58dc5a0e2f7ae3f035c
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-07 22:33:13 +0200

    Deduplicate DNS servers before shuffling

--- Module/Tests/ResolveDns.Tests.ps1
+++ Module/Tests/ResolveDns.Tests.ps1
@@ -4,4 +4,9 @@ Describe 'Resolve-Dns cmdlet' {
     It 'Fails when TimeOut is less than or equal to zero' {
         { Resolve-Dns -Name 'example.com' -TimeOut 0 -ErrorAction Stop } | Should -Throw -ExceptionType System.ArgumentOutOfRangeException
     }
+
+    It 'Removes duplicate servers before processing' {
+        $result = Resolve-Dns -Name 'example.com' -Server @('127.0.0.1','127.0.0.1') -AllServers -FullResponse -TimeOut 10 -ErrorAction SilentlyContinue
+        $result.Count | Should -Be 1
+    }
 }
