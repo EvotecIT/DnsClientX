17d40629bdd3b82f24471c8996b4d79b6e3de1cb
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-07 17:55:27 +0200

    Run DoT handshake test sequentially

--- DnsClientX.Tests/DnsWireResolveDotTests.cs
+++ DnsClientX.Tests/DnsWireResolveDotTests.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xunit;
 
 namespace DnsClientX.Tests {
+    [Collection("NoParallel")]
     public class DnsWireResolveDotTests {
         private static int GetFreePort() {
             TcpListener listener = new TcpListener(IPAddress.Loopback, 0);
