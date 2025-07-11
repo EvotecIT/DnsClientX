a62ac357f0797b4db74bacdcca004af29d799920
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-11 23:01:06 +0200

    Improve test to match coding style

--- DnsClientX.Tests/MaxConnectionsPerServerTests.cs
+++ DnsClientX.Tests/MaxConnectionsPerServerTests.cs
@@ -6,9 +6,9 @@ namespace DnsClientX.Tests {
     public class MaxConnectionsPerServerTests {
         [Fact]
         public void HandlerUsesConfigurationValue() {
-            using var client = new ClientX(DnsEndpoint.Cloudflare);
-            var handlerField = typeof(ClientX).GetField("handler", BindingFlags.NonPublic | BindingFlags.Instance)!;
-            var handler = (HttpClientHandler)handlerField.GetValue(client)!;
+            using ClientX client = new ClientX(DnsEndpoint.Cloudflare);
+            FieldInfo handlerField = typeof(ClientX).GetField("handler", BindingFlags.NonPublic | BindingFlags.Instance)!;
+            HttpClientHandler handler = (HttpClientHandler)handlerField.GetValue(client)!;
             Assert.Equal(client.EndpointConfiguration.MaxConnectionsPerServer, handler.MaxConnectionsPerServer);
         }
     }
