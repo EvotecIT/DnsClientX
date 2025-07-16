b092ac2e7ba7d0aab9bc38fc89237416b8138633
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-16 11:07:27 +0200

    Mark invalid referral test non-parallel

--- DnsClientX.Tests/ResolveRootInvalidReferralTests.cs
+++ DnsClientX.Tests/ResolveRootInvalidReferralTests.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xunit;
 
 namespace DnsClientX.Tests {
+    [Collection("NoParallel")]
     public class ResolveRootInvalidReferralTests {
         private static byte[] EncodeName(string name) {
             using var ms = new System.IO.MemoryStream();
